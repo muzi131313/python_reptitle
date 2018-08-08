07aaa094f113a0615cdfe461c5806cf3b6d09b44
李彦风 <liyanfeng@liyanfengdeMacBook-Pro.local>
2018-08-08 15:06:36 +0800

    更改为old地址

--- grab_images/meizi.py
+++ grab_images/meizi.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import os
 
 
-folder = os.getcwd()[:-4] + 'images/meizi/'
+folder = os.getcwd()[:-4] + 'images/meizi_old/'
 #获取此py文件路径，在此路径选创建在new_folder文件夹中的test文件夹
 
 if not os.path.exists(folder):
@@ -13,7 +13,7 @@ if not os.path.exists(folder):
 headers = {
     'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"
 } ##浏览器请求头（大部分网站没有这个请求头会报错、请务必加上哦）
-all_url = 'http://www.mzitu.com/all'  ##开始的URL地址
+all_url = 'http://www.mzitu.com/old'  ##开始的URL地址(all)
 start_html = requests.get(all_url, headers=headers)  ##使用requests中的get方法来获取all_url(就是：http://www.mzitu.com/all这个地址)的内容 headers为上面设置的请求头、请务必参考requests官方文档解释
 # print(start_html.text) ##打印出start_html (请注意，concent是二进制的数据，一般用于下载图片、视频、音频、等多媒体内容是才使用concent, 对于打印网页内容请使用text)
 Soup = BeautifulSoup(start_html.text, 'lxml') ##使用BeautifulSoup来解析我们获取到的网页（‘lxml’是指定的解析器 具体请参考官方文档哦）
